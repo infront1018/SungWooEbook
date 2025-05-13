f975041b47917248e098ce39adb39b780958a323
MEKIM <ckcdfg1004@naver.com>
2025-05-13 14:23:38 +0900

    250513_배경, 내비게이션 바 수정

--- app/src/main/java/com/sungwoobook/ebook/MainActivity.java
+++ app/src/main/java/com/sungwoobook/ebook/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
