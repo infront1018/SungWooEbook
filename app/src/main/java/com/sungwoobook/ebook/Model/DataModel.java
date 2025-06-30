6d780ffb9b9679e36c23c503fe8a2f3c327230cf
MEKIM <ckcdfg1004@naver.com>
2025-06-30 11:11:39 +0900

    테스트4

--- app/src/main/java/com/sungwoobook/ebook/Model/DataModel.java
+++ app/src/main/java/com/sungwoobook/ebook/Model/DataModel.java
@@ -2,7 +2,7 @@ package com.sungwoobook.ebook.Model;
 
 import java.util.List;
 
-public class DataModel {
+public class DataModel { // data 모델
     private List<ContentModel> contents;
 
     public DataModel(List<ContentModel> contents) {
